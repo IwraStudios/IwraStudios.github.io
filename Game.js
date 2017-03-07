1b3c3c63da789dfd41b1e40b3257aba5ed6beca5
IwraStudios <IwraStudios@users.noreply.github.com>
2017-03-07 11:43:57 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -14,19 +14,19 @@ window.onload = Init;
 var Ease = createjs.Ease;
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 37) {
-	if(hitmap[(World_Container.x/64)+1][(World_Container.y/64)] != 1){
+	if(hitmap[((-1*World_Container.x)/64)+1][(World_Container.y/64)] != 1){
 		createjs.Tween.get(World_Container).to({x:(World_Container.x+64).clamp(-10000,0)}, 500, Ease.linear);
 	    }
     }else if(event.keyCode == 39) {
-	if(hitmap[(World_Container.x/64)-1][(World_Container.y/64)] != 1){
+	if(hitmap[((-1*World_Container.x)/64)-1][(World_Container.y/64)] != 1){
         createjs.Tween.get(World_Container).to({x:(World_Container.x-64).clamp(-10000,0)}, 500, Ease.linear);
 	}
     }else if(event.keyCode == 38){
-	 if(hitmap[(World_Container.x/64)][(World_Container.y/64) +1] != 1){
+	 if(hitmap[((-1*World_Container.x)/64)][(World_Container.y/64) +1] != 1){
 	 createjs.Tween.get(World_Container).to({y:(World_Container.y+64).clamp(-10000,0)}, 500, Ease.linear);   
 	 }
     }else if(event.keyCode == 40){
-	 if(hitmap[(World_Container.x/64)+1][(World_Container.y/64)-1] != 1){
+	 if(hitmap[((-1*World_Container.x)/64)+1][(World_Container.y/64)-1] != 1){
 	 createjs.Tween.get(World_Container).to({y:(World_Container.y-64).clamp(-10000,0)}, 500, Ease.linear);
 	 }
     }
@@ -129,7 +129,7 @@ function tp(x,y){
 		return;
 	}
 	console.log("tp" + (x).toString() + " " + (y).toString());
-	World_Container.setTransform(64*x,64*y);
+	World_Container.setTransform(-64*x,-64*y);
 	stage.update();
 }
  
