fae14521445778acea011e733eda80fc4990fd06
IwraStudios <IwraStudios@users.noreply.github.com>
2017-03-16 11:30:08 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -49,6 +49,7 @@ document.addEventListener('keydown', function(event) {
     var rchartx = chartx - 5;
     var rcharty = charty - 5;
 	createjs.Tween.get(World_Container).to({x:(rchartx* -1 * grid).clamp(-10000,288), y:(rcharty* -1 * grid).clamp(-10000,288)}, 350, Ease.Linear).wait(50).call(handleComplete);
+	createjs.Tween.get(char).to({x:(chartx *-1 * grid).clamp(-10000,0), y:(charty* -1 * grid).clamp(-10000,0)}, 350, Ease.Linear.wait(50);
 	stage.update();
 	ExecTile(chartx,charty);
 });
@@ -160,12 +161,12 @@ function Init(){
 	}
 	char = new createjs.Bitmap("./images/char1/0.png");
 	char.image = chararr[0].image;
-	char.x = charx;
-	char.y = chary;
+	char.x = -1 * grid *chartx;
+	char.y = -1 * grid *charty;
 	char.name = "./images/char1/0.png;3";
 	stage.addChild(World_Container);
 
-	stage.addChild(char);
+	World_Container.addChild(char);
 	World_Container.scaleX = 2;
 	World_Container.scaleY = 2;
 	ticker = createjs.Ticker.addEventListener("tick", handleTick);
@@ -228,6 +229,7 @@ function tp(x,y){
 	chartx = x + 5;
 	charty = y + 5;
 	World_Container.setTransform(-1 * grid *x,-1* grid *y);
+	char.setTransform(-1 * grid *chartx,-1*gird*charty);
 	stage.update();
 }
 
