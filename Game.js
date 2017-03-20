cbe5c393c7b74e8a6f940100e4436c0a464c0e65
IwraStudios <IwraStudios@users.noreply.github.com>
2017-03-20 14:18:20 +0100

    Update Game.js

--- Game.js
+++ Game.js
@@ -229,6 +229,9 @@ function tp(x,y){
 	chartx = x + 5;
 	charty = y + 5;
 	World_Container.setTransform(-1 * grid *x,-1* grid *y);
+	var rchartx = chartx - 5;
+        var rcharty = charty - 5;
+	createjs.Tween.get(World_Container).to({x:(rchartx* -1 * grid).clamp(-10000,288), y:(rcharty* -1 * grid).clamp(-10000,288)}, 350, Ease.Linear).wait(50).call(handleComplete);
 	stage.update();
 }
 
