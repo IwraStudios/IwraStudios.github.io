e066f2c920ac1d00bca48e68030ed54b21b2d741
IwraStudios <IwraStudios@users.noreply.github.com>
2017-04-12 12:22:03 +0200

    Create Game2.js

---
+++ Game2.js
@@ -0,0 +1,22 @@
+var stage;
+var ticker;
+
+window.onload = Init;
+//Useless variable
+var Ease = createjs.Ease;
+document.addEventListener('keydown', pdown);
+
+function Init(){
+	stage = new createjs.Stage("GameCanvas");
+
+	ticker = createjs.Ticker.addEventListener("tick", handleTick);
+	ticker.framerate = 30;
+  
+  
+}
+
+
+function newGame(){
+  	var block = new createjs.Shape().set({x:Math.floor(Math.random() * 580) + 10, y:Math.floor(Math.random() * 3)*100, scaleX:1});
+    block.graphics.beginFill("green").drawRect(0,0,50,30);
+}
